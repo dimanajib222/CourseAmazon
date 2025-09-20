05d7b3ee874d8e30e86ef239c2b74f5f0c014429
Dima <dima@najib.com>
2025-09-20 04:41:59 -0800

    adding the migrationapp

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.Data;
+using Amazon.Domain;
+using System.Text.Json;
+
+namespace MigrationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadDefaultEmployeesData();
+            
+        
+        }
+
+        private static void LoadDefaultEmployeesData()
+        {
+            // read the Data.Json file  
+            string contentsAsString = File.ReadAllText("Data.json");
+            // change the String into a List<Employee> (Deserialize)
+            var employees = JsonSerializer.Deserialize<List<Employee>>(contentsAsString);
+            //call Context to Adding the new employees
+            using (var context = new AmazonContext())
+            { 
+            //add bulk employee
+            context.Employees.AddRange(employees);
+            context.SaveChanges();
+            
+            }
+        }
+
+
+    }
+
+
+
+}
